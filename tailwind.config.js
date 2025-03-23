29d8d64d911db809d8bc4e804474d279f35bcd48
Geethika <geethikaabharoth@gmail.com>
2025-03-23 19:20:27 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+      extend: {
+        colors: {
+          strongCyan: 'hsl(171, 66%, 44%)',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
